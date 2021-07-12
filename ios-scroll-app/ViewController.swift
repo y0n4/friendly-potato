0d4350adeaca5310e2738790bbff92fd99e748d7
y0n4 <04l.yona@gmail.com>
2021-07-12 02:49:09 -0400

    Initial Commit

---
+++ ios-scroll-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-scroll-app
+//
+//  Created by yona nagayama on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
